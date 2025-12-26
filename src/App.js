3d1641ab195b5d6f608746305cb0036ca6538ad2
raytr <giaotran2001lk@gmail.com>
2025-12-26 15:54:46 +0700

    feat: update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim() !== "") {
+      setTodos([...todos, text]);
+      setText("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, idx) => (
+          <li key={idx}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
